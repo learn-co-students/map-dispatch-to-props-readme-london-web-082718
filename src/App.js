f4a194c90fb4f6c615d98e2785efec37c57f55e7
klbennett <thestylishkidsintheriot@gmail.com>
2018-11-18 18:56:19 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick = event => {
+    this.props.addItem()
   }
 
   render() {
+    debugger
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -21,10 +22,25 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
 
-export default connect(mapStateToProps)(App);
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem());
+//     }
+//   };
+// };
+
+//The second argument of connect will accept a function (as we've seen) or an object. If we pass in a function, mapDispatchToProps(), we must incorporate dispatch. If we pass in an object, connect handles this for us!
+
+
+export default connect(
+  state => ({ items: state.items }),
+  { addItem }
+)(App);
